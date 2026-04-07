afb729351a58edcef5c5b14126660645a18dec26
onspli <ondrej.splichal@gmail.com>
2026-04-07 23:18:52 +0200

    refactor: update barrel exports structure

---
+++ lib/entities/entities.dart
@@ -0,0 +1,93 @@
+// Entity models: Calendar
+export 'calendar/calendar_event.dart';
+export 'calendar/calendar_event_attendance.dart';
+export 'calendar/properties/attendee/calendar_attendee.dart';
+export 'calendar/properties/attendee/calendar_attendee_expect_reply.dart';
+export 'calendar/properties/attendee/calendar_attendee_kind.dart';
+export 'calendar/properties/attendee/calendar_attendee_mail_to.dart';
+export 'calendar/properties/attendee/calendar_attendee_name.dart';
+export 'calendar/properties/attendee/calendar_attendee_participation_status.dart';
+export 'calendar/properties/attendee/calendar_attendee_role.dart';
+export 'calendar/properties/calendar_duration.dart';
+export 'calendar/properties/calendar_event_status.dart';
+export 'calendar/properties/calendar_extension_fields.dart';
+export 'calendar/properties/calendar_free_busy_status.dart';
+export 'calendar/properties/calendar_organizer.dart';
+export 'calendar/properties/calendar_priority.dart';
+export 'calendar/properties/calendar_privacy.dart';
+export 'calendar/properties/calendar_sequence.dart';
+export 'calendar/properties/event_id.dart';
+export 'calendar/properties/event_method.dart';
+export 'calendar/properties/event_status.dart';
+export 'calendar/properties/mail_address.dart';
+export 'calendar/properties/recurrence_rule/day_of_week.dart';
+export 'calendar/properties/recurrence_rule/recurrence_rule.dart';
+export 'calendar/properties/recurrence_rule/recurrence_rule_count.dart';
+export 'calendar/properties/recurrence_rule/recurrence_rule_frequency.dart';
+export 'calendar/properties/recurrence_rule/recurrence_rule_interval.dart';
+export 'calendar/properties/recurrence_rule/recurrence_rule_rscale.dart';
+export 'calendar/properties/recurrence_rule/recurrence_rule_skip.dart';
+// Entity models: Capabilities
+export 'capability/calendar_event_capability.dart';
+export 'capability/core_capability.dart';
+export 'capability/default_capability.dart';
+export 'capability/empty_capability.dart';
+export 'capability/mail_capability.dart';
+export 'capability/mdn_capability.dart';
+export 'capability/submission_capability.dart';
+export 'capability/vacation_capability.dart';
+export 'capability/web_socket_ticket_capability.dart';
+export 'capability/websocket_capability.dart';
+// Entity models: Core
+export 'core/account.dart';
+export 'core/account_id.dart';
+export 'core/capability_identifier.dart';
+export 'core/capability_properties.dart';
+export 'core/id.dart';
+export 'core/session.dart';
+export 'core/state.dart';
+export 'core/unsigned_int.dart';
+export 'core/user_name.dart';
+export 'core/utc_date.dart';
+// Entity models: Email
+export 'email/address.dart';
+export 'email/delivery_status.dart';
+export 'email/email.dart';
+export 'email/email_address.dart';
+export 'email/email_body_part.dart';
+export 'email/email_body_properties.dart';
+export 'email/email_body_value.dart';
+export 'email/email_comparator.dart';
+export 'email/email_comparator_property.dart';
+export 'email/email_filter_condition.dart';
+export 'email/email_header.dart';
+export 'email/email_submission.dart';
+export 'email/email_submission_id.dart';
+export 'email/envelope.dart';
+export 'email/individual_header_identifier.dart';
+export 'email/keyword_identifier.dart';
+export 'email/public_asset.dart';
+export 'email/search_snippet.dart';
+// Entity models: Identity
+export 'identity/identity.dart';
+// Entity models: Mailbox
+export 'mailbox/mailbox.dart';
+export 'mailbox/mailbox_filter_condition.dart';
+export 'mailbox/mailbox_rights.dart';
+export 'mailbox/namespace.dart';
+// Entity models: MDN
+export 'mdn/disposition.dart';
+export 'mdn/mdn.dart';
+// Entity models: Push
+export 'push/encryption_key.dart';
+export 'push/push_subscription.dart';
+export 'push/state_change.dart';
+export 'push/type_state.dart';
+// Entity models: Quota
+export 'quota/data_types.dart';
+export 'quota/quota.dart';
+// Entity models: Thread
+export 'thread/thread.dart';
+// Entity models: Vacation
+export 'vacation/vacation_id.dart';
+export 'vacation/vacation_response.dart';
---
+++ lib/methods/methods.dart
@@ -0,0 +1,66 @@
+// Methods: Calendar
+export 'calendar/attendance/get_calendar_event_attendance_method.dart';
+export 'calendar/attendance/get_calendar_event_attendance_response.dart';
+export 'calendar/parse/calendar_event_parse_method.dart';
+export 'calendar/parse/calendar_event_parse_response.dart';
+export 'calendar/reply/calendar_event_accept_method.dart';
+export 'calendar/reply/calendar_event_accept_response.dart';
+export 'calendar/reply/calendar_event_counter_accept_method.dart';
+export 'calendar/reply/calendar_event_maybe_method.dart';
+export 'calendar/reply/calendar_event_maybe_response.dart';
+export 'calendar/reply/calendar_event_reject_method.dart';
+export 'calendar/reply/calendar_event_reject_response.dart';
+// Methods: Email
+export 'email/changes_email_method.dart';
+export 'email/changes_email_response.dart';
+export 'email/get_email_method.dart';
+export 'email/get_email_response.dart';
+export 'email/get_public_asset_method.dart';
+export 'email/get_public_asset_response.dart';
+export 'email/parse_email_method.dart';
+export 'email/parse_email_response.dart';
+export 'email/query_email_method.dart';
+export 'email/query_email_response.dart';
+export 'email/search_snippet_get_method.dart';
+export 'email/search_snippet_get_response.dart';
+export 'email/set_email_method.dart';
+export 'email/set_email_response.dart';
+export 'email/set_email_submission_method.dart';
+export 'email/set_email_submission_response.dart';
+export 'email/set_public_asset_method.dart';
+export 'email/set_public_asset_response.dart';
+// Methods: Identity
+export 'identity/get_identity_method.dart';
+export 'identity/get_identity_response.dart';
+export 'identity/set_identity_method.dart';
+export 'identity/set_identity_response.dart';
+// Methods: Mailbox
+export 'mailbox/changes/changes_mailbox_method.dart';
+export 'mailbox/changes/changes_mailbox_response.dart';
+export 'mailbox/clear/clear_mailbox_method.dart';
+export 'mailbox/clear/clear_mailbox_response.dart';
+export 'mailbox/get/get_mailbox_method.dart';
+export 'mailbox/get/get_mailbox_response.dart';
+export 'mailbox/query/query_mailbox_method.dart';
+export 'mailbox/query/query_mailbox_response.dart';
+export 'mailbox/set/set_mailbox_method.dart';
+export 'mailbox/set/set_mailbox_response.dart';
+// Methods: MDN
+export 'mdn/mdn_send_method.dart';
+export 'mdn/mdn_send_response.dart';
+// Methods: Push
+export 'push/get_push_subscription_method.dart';
+export 'push/get_push_subscription_response.dart';
+export 'push/set_push_subscription_method.dart';
+export 'push/set_push_subscription_response.dart';
+// Methods: Quota
+export 'quota/get_quota_method.dart';
+export 'quota/get_quota_response.dart';
+// Methods: Thread
+export 'thread/get_thread_method.dart';
+export 'thread/get_thread_response.dart';
+// Methods: Vacation
+export 'vacation/get_vacation_method.dart';
+export 'vacation/get_vacation_response.dart';
+export 'vacation/set_vacation_method.dart';
+export 'vacation/set_vacation_response.dart';
