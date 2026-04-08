0aab9b7a426b305be1d1ec432ead43f4d0332c37
onspli <ondrej.splichal@gmail.com>
2026-04-09 00:20:16 +0200

    refactor: remove internal TypeStateConverter from public API

--- lib/entities/push/state_change.dart
+++ lib/entities/push/state_change.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:jmap_dart_client/entities/core/account_id.dart';
+import 'package:jmap_dart_client/entities/core/id.dart';
 import 'package:jmap_dart_client/entities/push/type_state.dart';
-import 'package:jmap_dart_client/src/converters/type_state_converter.dart';
 
 class StateChange with EquatableMixin {
   final String type;
@@ -11,17 +11,41 @@ class StateChange with EquatableMixin {
 
   factory StateChange.fromJson(
     Map<String, dynamic> json, {
-    TypeStateConverter? converter,
+    Map<AccountId, TypeState Function(Map<String, dynamic>)>?
+    customTypeStateConverters,
   }) {
-    converter ??= TypeStateConverter.defaultConverter;
+    final converter = _TypeStateConverter(customTypeStateConverters);
     return StateChange(
       json['@type'] as String,
       (json['changed'] as Map<String, dynamic>).map(
-        (key, value) => converter!.convert(key, value),
+        (key, value) => converter.convert(key, value),
       ),
     );
   }
 
   @override
   List<Object?> get props => [type, changed];
+}
+
+class _TypeStateConverter {
+  final Map<AccountId, TypeState Function(Map<String, dynamic>)> _converters =
+      {};
+
+  _TypeStateConverter([
+    Map<AccountId, TypeState Function(Map<String, dynamic>)>? customConverters,
+  ]) {
+    if (customConverters != null) {
+      _converters.addAll(customConverters);
+    }
+  }
+
+  MapEntry<AccountId, TypeState> convert(String key, dynamic value) {
+    final identifier = AccountId(Id(key));
+    final converter = _converters[identifier];
+    if (converter != null) {
+      return MapEntry(identifier, converter.call(value));
+    } else {
+      return MapEntry(identifier, TypeState(value));
+    }
+  }
 }
